09ad88c14fe509b1d0c2be2d2eb32c9f636c1281
RV <rauno.valk@voco.ee>
2024-11-29 16:55:02 +0200

    Create new Todo from user data

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import todoRoutes from './routes/todos.js'
+
+
+const app = express()
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/todos', todoRoutes)
+
+app.listen(3009, () =>{
+    console.log('Server is connected at port 3009')
+})

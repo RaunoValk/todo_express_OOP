58fec532d75deb2cb852da6f0173736a6122616f
RV <rauno.valk@voco.ee>
2024-12-13 17:04:30 +0200

    Delete

--- controllers/todos.js
+++ controllers/todos.js
@@ -16,17 +16,60 @@ class todoController {
     }
     async initTodos(){
         const todosData = await fileManager.readFile('./data/todos.json')
-        // if data is ok - add file content to array
+        
         if(todosData !== null){
             this.TODOS = todosData
         } else  {
-            this.TODOS = [] // if we do not get data from file create an empty array
+            this.TODOS = []
         }
     }
 
     getTodos(req, res){
         res.json({tasks: this.TODOS})
     }
+
+    async updateTodo(req,res){
+        const todoId = req.params.id 
+        const updatedTask = req.body.task
+
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if(todoIndex < 0){
+            res.json({
+                message: 'Could not find todo with such index'
+            } )
+            throw new Error('Could not find todo')
+        }
+        
+        this.TODOS[todoIndex] = new Todo (this.TODOS[todoIndex].id, updatedTask)
+            res.json({
+                message: 'todo is updated',
+                updatedTask: this.TODOS[todoIndex] 
+            } ) 
+    }
+    async deleteTodo(req,res){
+        const todoId = req.params.id 
+        const deletedTask = req.body.task
+
+        let todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if(todoIndex < 0){
+            res.json({
+                message: 'Could not find todo with such index'
+            } )
+            throw new Error('Could not find todo')
+        }
+     else{ 
+        this.TODOS[todoIndex] = new Todo (this.TODOS[todoIndex].id, deletedTask)
+
+            res.json({
+                message: 'todo is deleted',
+                deletedTask: this.TODOS[todoIndex] 
+            } )
+            this.TODOS.splice(todoIndex, 1) 
+        }    
+     }
+ 
 }
 
 export const TodoController = new todoController
